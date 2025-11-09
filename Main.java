f9b986912f1c5a2a313dccb54d5786971e6cdf3d
Mohithyalamarthi2003 <yalamarthymohithchowdhary@gmail.com>
2025-11-09 19:44:32 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        String str="Mohith";
+        System.out.println(str);
+        for(int i=0;i<=str.length()-1;i++)
+        {
+ String rev=str.charAt(str.length()-1-i);
+                        
+        }
+        System.out.println(rev);
+
+    }
+}
